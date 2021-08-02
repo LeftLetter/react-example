610e25637c056d52979d60c80d0ad12e2bf2369b
LeftLetter <zmsh_white@yahoo.co.jp>
2021-08-02 13:56:39 +0000

    propsエラー修正

--- ui/src/components/pages/TopPage.tsx
+++ ui/src/components/pages/TopPage.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
+import { useCardState } from '../../hooks/useCardState'
 import { Cards } from '../../utils/types/Cards'
 import { Header } from '../molecules/Header'
 import { ItemCard } from '../organisms/ItemCard'
+
 type Props = { cards: Cards } & ContainerProps
 
 type ContainerProps = {
@@ -35,15 +37,7 @@ export const StyledComponent = styled(Component)`
 `
 
 const Container: React.FC<ContainerProps> = (props) => {
-  // const [cards, setCards] = useCardState()
-  const cards: Cards = [
-    {
-      id: '0',
-      title: 'Very Long Long Long Long Long Long Title',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    },
-  ]
+  const [cards, setCards] = useCardState()
 
   return <StyledComponent {...props} cards={cards} />
 }
--- ui/src/contexts/cardContext.tsx
+++ ui/src/contexts/cardContext.tsx
@@ -4,6 +4,7 @@ import { Cards } from '../utils/types/Cards'
 export const CardContext = React.createContext<Cards>([])
 
 export const CardUpdateContext =
+  // ここは要調査
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   React.createContext<React.Dispatch<React.SetStateAction<Cards>>>(undefined)
@@ -12,13 +13,13 @@ type Props = {
   children: React.ReactNode
 }
 
-export const CardContextProvider: React.VFC<Props> = (children) => {
+export const CardContextProvider: React.VFC<Props> = (props) => {
   const [cards, setCards] = useState<Cards>([])
 
   return (
     <CardContext.Provider value={cards}>
       <CardUpdateContext.Provider value={setCards}>
-        {children}
+        {props.children}
       </CardUpdateContext.Provider>
     </CardContext.Provider>
   )
