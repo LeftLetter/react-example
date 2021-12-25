639a9dc05a0bb7710e2edbefba7e02686804d8dc
LeftLetter <zmsh_white@yahoo.co.jp>
2021-12-25 05:43:18 +0000

    MSWを追加

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './styles/index.scss'
+;(async () => {
+  if (process.env.NODE_ENV === 'development') {
+    const { worker } = await import('./mocks/browser.js')
+    worker.start()
+  }
+})()
 
 ReactDOM.render(
   <React.StrictMode>
